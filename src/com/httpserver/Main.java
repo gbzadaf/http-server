cfacb8c74fe7d42ef6e43550c31d061f1e546015
gbzadaf <fonsecagb23@gmail.com>
2026-04-22 11:40:50 -0300

    Project created and ServerSocket

---
+++ src/com/httpserver/Main.java
@@ -0,0 +1,24 @@
+package com.httpserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Servidor rodando em http://localhost:" + PORT);
+
+        while (true) {
+
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Nova conexão: " + clientSocket.getInetAddress());
+
+        }
+    }
+
+}
